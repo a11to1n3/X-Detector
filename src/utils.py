4acbd4cfeca19ba034c1254b9c31ecf26e761889
a11t01n3 <duyanhpham@outlook.com>
2021-12-26 12:40:48 +0100

    unlisted class bounding coordinates

--- src/utils.py
+++ src/utils.py
@@ -120,7 +120,7 @@ def find_bounding_boxes_per_class(CAM_explainer, img_path, class_names, class_la
         ymins = [min(np.array(X)[clustering.labels_ == l,1]) for l in np.unique(clustering.labels_) if l != -1]
         ymaxs = [max(np.array(X)[clustering.labels_ == l,1]) for l in np.unique(clustering.labels_) if l != -1]
 
-        class_boxes[class_oi] = [xmins, ymins, xmaxs, ymaxs]
+        class_boxes[class_oi] = [xmins[0], ymins[0], xmaxs[0], ymaxs[0]]
 
         if plot:
             overlay = overlay * (overlay > 0.5).astype(np.uint8)
