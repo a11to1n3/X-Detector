5383cf5a5d66d4d2505368bad4f8bc8d7b27b180
a11t01n3 <duyanhpham@outlook.com>
2021-12-26 12:01:45 +0100

    added torch vision utils

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import copy
 import pandas as pd
 import cv2 as cv
 import numpy as np
+from torchvision import transforms, utils, models
 from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
 
