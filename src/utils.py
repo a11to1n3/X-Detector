a7aa87055cd0a37dda1b6208f2d4e76b1d7e33f9
a11t01n3 <duyanhpham@outlook.com>
2021-12-26 12:47:35 +0100

    supporting more than 1 box per class

--- src/utils.py
+++ src/utils.py
@@ -89,6 +89,7 @@ def find_bounding_boxes_per_class(CAM_explainer, img_path, class_names, class_la
         ax = fig.gca()
 
     class_boxes = {}
+    [class_boxes[class_oi] = [] for class_oi in list(strToLabel.keys())]
     for class_oi in list(strToLabel.keys()):
         img = cv.imread(img_path)
         data_transform = transforms.Compose([
@@ -120,7 +121,7 @@ def find_bounding_boxes_per_class(CAM_explainer, img_path, class_names, class_la
         ymins = [min(np.array(X)[clustering.labels_ == l,1]) for l in np.unique(clustering.labels_) if l != -1]
         ymaxs = [max(np.array(X)[clustering.labels_ == l,1]) for l in np.unique(clustering.labels_) if l != -1]
 
-        class_boxes[class_oi] = [xmins[0], ymins[0], xmaxs[0], ymaxs[0]]
+        class_boxes[class_oi].append([xmins[0], ymins[0], xmaxs[0], ymaxs[0]])
 
         if plot:
             overlay = overlay * (overlay > 0.5).astype(np.uint8)
